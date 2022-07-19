edc899647e21f5d2ff78de30679ae3db1b50f9c6
cod-y-clark <cody66clark@gmail.com>
2022-07-18 19:30:42 -0500

    adding routing

---
+++ components/User.js
@@ -0,0 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function User({
+  image,
+  name,
+  email,
+  lastLogin,
+}) {
+  return (
+
+    <div>
+      <img src={image} alt={name} />
+      <h1>{name}</h1>
+      <h2>{email}</h2>
+      <h3>{lastLogin}</h3>
+    </div>
+  );
+}
+
+User.propTypes = {
+  image: PropTypes.string,
+  name: PropTypes.string,
+  email: PropTypes.string,
+  lastLogin: PropTypes.string,
+};
+
+User.defaultProps = {
+  image: 'https://upload.wikimedia.org/wikipedia/commons/1/17/Tomi_Adeyemi_2020.JPG',
+  name: 'Tomi Adeyemi',
+  email: 'tadeyemi@email.com',
+  lastLogin: '2022-07-15',
+};
+
+export default User;
---
+++ pages/author/edit/[firebaseKey].js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function editAuthor() {
+  return <div>Edit Authors</div>;
+}
