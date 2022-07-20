e0e3f0087535280be81b94d8b0e275b92f288063
cod-y-clark <cody66clark@gmail.com>
2022-07-19 21:21:36 -0500

    Adding create/delete for Authors

--- components/AuthorCard.js
+++ components/AuthorCard.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Card, Button } from 'react-bootstrap';
+import { deleteSingleAuthor } from '../api/authorData';
 
-function AuthorCard({
-  firstName,
-  lastName,
-  email,
-}) {
+export default function AuthorCard({ authorObj, onUpdate }) {
+  const deleteAuthorAndUpdate = () => {
+    deleteSingleAuthor(authorObj.firebaseKey).then(onUpdate);
+  };
   return (
-    <div>
-      <h1>{firstName}</h1>
-      <h1>{lastName}</h1>
-      <h2>{email}</h2>
-    </div>
+    <>
+      <Card style={{ width: '18rem', margin: '10px' }}>
+        <Card.Body>
+          <Card.Title>{authorObj.first_name} {authorObj.last_name}</Card.Title>
+          <p>Email: {authorObj.email}</p>
+          <p className="card-text bold">{authorObj.favorite ? 'Favorite!' : 'Not a Favorite' }</p>
+          <Button variant="danger" onClick={deleteAuthorAndUpdate} className="m-2">
+            DELETE
+          </Button>
+        </Card.Body>
+      </Card>
+    </>
   );
 }
 
 AuthorCard.propTypes = {
-  firstName: PropTypes.string,
-  lastName: PropTypes.string,
-  email: PropTypes.string,
+  authorObj: PropTypes.shape({
+    name: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    email: PropTypes.string,
+    favorite: PropTypes.bool,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
-
-AuthorCard.defaultProps = {
-  firstName: 'Cody',
-  lastName: 'Clark',
-  email: 'cody@email.com',
-};
-
-export default AuthorCard;
--- pages/authors.js
+++ pages/authors.js
@@ -1,6 +1,26 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { React, useState, useEffect } from 'react';
+import { getAuthors } from '../api/authorData';
 import AuthorCard from '../components/AuthorCard';
+import { useAuth } from '../utils/context/authContext';
 
 export default function Authors() {
-  return <div><AuthorCard /></div>;
+  const [authors, setAuthors] = useState([]);
+  const { user } = useAuth();
+
+  const getAllTheAuthors = () => {
+    getAuthors(user.uid).then(setAuthors);
+  };
+
+  useEffect(() => {
+    getAllTheAuthors();
+  }, []);
+
+  return (
+    <div>
+      {authors.map((author) => (
+        <AuthorCard key={author.firebaseKey} authorObj={author} onUpdate={getAllTheAuthors} />
+      ))}
+    </div>
+  );
 }
